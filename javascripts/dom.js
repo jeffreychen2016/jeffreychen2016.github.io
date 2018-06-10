eac2eded8079b4578dd13e66bec7a37382eeb3e7
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-10 17:21:36 -0500

    added three buttons to blog cards

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -2,12 +2,17 @@ const printBlogs = (blogData) => {
   let domString = '';
   for (let i = 0; i < blogData.length; i++) {
     domString += `<div>`;
-    domString += `<h2 class="float-left">${blogData[i].title}</h2>`;
-    domString += `<p class="float-right">${blogData[i].date}</p>`;
-    domString += `<p class="clear-float">${blogData[i].post}</p>`;
+    domString +=  `<h2 class="float-left">${blogData[i].title}</h2>`;
+    // domString += `<p class="float-right">${blogData[i].date}</p>`;
+    domString +=  `<div class="btn-group float-right" role="group" aria-label="...">`;
+    domString +=    `<button type="button" class="btn btn-default"><span class='glyphicon glyphicon-plus'></span>Add</button>`;
+    domString +=    `<button type="button" class="btn btn-default"><span class='glyphicon glyphicon-pencil'></span>Edit</button>`;
+    domString +=    `<button type="button" class="btn btn-default"><span class='glyphicon glyphicon-trash'></span>Delete</button>`;
+    domString +=  `</div>`;
+    domString +=  `<p class="clear-float">${blogData[i].post}</p>`;
     domString += `</div>`;
   }
-  $('#blog-container').append(domString);
+  $('#blog-container').html(domString);
 };
 
 module.exports = {
