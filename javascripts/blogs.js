bd8007ff17196aa93522ce6bf8d3ab6347d76017
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-10 14:22:21 -0500

    initialized firebase

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -1,35 +1,35 @@
 
-const writeToDom = (string,id)=>{
-    document.getElementById(id).innerHTML = string;
-}
+// const writeToDom = (string,id)=>{
+//     document.getElementById(id).innerHTML = string;
+// }
 
-const createBlogPosts = (data)=>{
-    let domString = '';
-    for(let i = 0; i < data.blogs.length; i++){
-        domString += `<div>`;
-        domString += `<h2 class="float-left">${data.blogs[i].title}</h2>`;
-        domString += `<p class="float-right">${data.blogs[i].date}</p>`;
-        domString += `<p class="clear-float">${data.blogs[i].post}</p>`;
-        domString += `</div>`;
-      }
-      writeToDom(domString,'blog-container');    
-}
+// const createBlogPosts = (data)=>{
+//     let domString = '';
+//     for(let i = 0; i < data.blogs.length; i++){
+//         domString += `<div>`;
+//         domString += `<h2 class="float-left">${data.blogs[i].title}</h2>`;
+//         domString += `<p class="float-right">${data.blogs[i].date}</p>`;
+//         domString += `<p class="clear-float">${data.blogs[i].post}</p>`;
+//         domString += `</div>`;
+//       }
+//       writeToDom(domString,'blog-container');
+// }
 
-function executeOnSuccess(){
-    const data = JSON.parse(this.responseText);
-    createBlogPosts(data);
-}
+// function executeOnSuccess(){
+//     const data = JSON.parse(this.responseText);
+//     createBlogPosts(data);
+// }
 
-function executeOnFail(){
-    console.log('XHR fails');
-}
+// function executeOnFail(){
+//     console.log('XHR fails');
+// }
 
-const runApplication = () => {
-    let xhrRequest = new XMLHttpRequest();
-    xhrRequest.addEventListener('load',executeOnSuccess);
-    xhrRequest.addEventListener("error", executeOnFail);
-    xhrRequest.open("GET","./db/blogs.json");
-    xhrRequest.send();
-}
+// const runApplication = () => {
+//     let xhrRequest = new XMLHttpRequest();
+//     xhrRequest.addEventListener('load',executeOnSuccess);
+//     xhrRequest.addEventListener("error", executeOnFail);
+//     xhrRequest.open("GET","./db/blogs.json");
+//     xhrRequest.send();
+// }
 
-runApplication();
+// runApplication();
--- javascripts/projects.js
+++ javascripts/projects.js
@@ -1,35 +1,35 @@
-const writeToDom = (string,id)=>{
-    document.getElementById(id).innerHTML = string;
-}
+// const writeToDom = (string,id)=>{
+//     document.getElementById(id).innerHTML = string;
+// }
 
-const createProjectCards = (data)=>{
-    let domString = '';
-    for(let i = 0; i < data.projects.length; i++){
-        domString += `<div>`;
-        domString += `<h2>${data.projects[i].title}</h2>`;
-        domString += `<img src ="${data.projects[i].imageUrl}">`;
-        domString += `<p>${data.projects[i].description}</p>`;
-        domString += `<p>${data.projects[i].githubUrl}</p>`;
-        domString += `</div>`;
-      }
-    writeToDom(domString,'project-container');    
-}
+// const createProjectCards = (data)=>{
+//     let domString = '';
+//     for(let i = 0; i < data.projects.length; i++){
+//         domString += `<div>`;
+//         domString += `<h2>${data.projects[i].title}</h2>`;
+//         domString += `<img src ="${data.projects[i].imageUrl}">`;
+//         domString += `<p>${data.projects[i].description}</p>`;
+//         domString += `<p>${data.projects[i].githubUrl}</p>`;
+//         domString += `</div>`;
+//       }
+//     writeToDom(domString,'project-container');
+// }
 
-function executeOnSuccess(){
-    const data = JSON.parse(this.responseText);
-    createProjectCards(data);
-}
+// function executeOnSuccess(){
+//     const data = JSON.parse(this.responseText);
+//     createProjectCards(data);
+// }
 
-function executeOnFail(){
-    console.log('XHR fails');
-}
+// function executeOnFail(){
+//     console.log('XHR fails');
+// }
 
-const runApplication = () => {
-    let xhrRequest = new XMLHttpRequest();
-    xhrRequest.addEventListener('load',executeOnSuccess);
-    xhrRequest.addEventListener("error", executeOnFail);
-    xhrRequest.open("GET","./db/projects.json");
-    xhrRequest.send();
-}
+// const runApplication = () => {
+//     let xhrRequest = new XMLHttpRequest();
+//     xhrRequest.addEventListener('load',executeOnSuccess);
+//     xhrRequest.addEventListener("error", executeOnFail);
+//     xhrRequest.open("GET","./db/projects.json");
+//     xhrRequest.send();
+// }
 
-runApplication();
+// runApplication();
