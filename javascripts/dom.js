e7240d64dfc8ad56299a9d1529492e55f1b84123
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-10-11 14:04:20 -0500

    Changed the printProjectPaginationController method to dynamically generates the page number

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -12,6 +12,7 @@ const generateRondomNumbers = (ArrayLength,numbersNeeded) => {
 };
 
 const printProjectPaginationController = (projectData) => {
+  const numberOfPageNeeded = Math.ceil(projectData.length / 6);
   let domString = '';
   domString += '<nav aria-label="Page navigation example" class="pagination-container">';
   domString +=   '<ul class="pagination">';
@@ -21,9 +22,9 @@ const printProjectPaginationController = (projectData) => {
   domString +=         '<span class="sr-only">Previous</span>';
   domString +=       '</a>';
   domString +=     '</li>';
-  domString +=     '<li class="page-item"><a class="page-link">1</a></li>';
-  domString +=     '<li class="page-item"><a class="page-link">2</a></li>';
-  domString +=     '<li class="page-item"><a class="page-link">3</a></li>';
+  for (let i = 0; i < numberOfPageNeeded; i++) {
+    domString +=     `<li class="page-item"><a class="page-link">${i + 1}</a></li>`;
+  };
   domString +=       '<li class="page-item">';
   domString +=         '<a class="page-link" href="#" aria-label="Next">';
   domString +=           '<span aria-hidden="true">&raquo;</span>';
